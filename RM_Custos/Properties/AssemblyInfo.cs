1ce7f37ae54c8a83d230ba3efc17695bf70b0c56
roberto.firmino <roberto.firmino@POAD040100320>
2018-05-11 11:09:35 -0300

    Add project files.

---
+++ RM_Custos/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RM_Custos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RM_Custos")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c51a4d84-8dfb-4ffb-82c5-73b45372ed88")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RM_Custos/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Xml;
+using Xunit;
+
+
+namespace RM_Custos
+{
+	public class UnitTest1
+	{
+
+		public string Envelope = @"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:br='http://www.totvs.com.br/br/'>
+			   <soapenv:Header/>
+			   <soapenv:Body>
+				  <br:ReadRecordAuth>
+					 <br:DataServerName>GchPacienteData</br:DataServerName>
+					 <br:PrimaryKey>1;1;100630</br:PrimaryKey>
+					 <br:Contexto>CODCOLIGADA=1;CODSISTEMA=0;CODUSUARIO=mestre</br:Contexto>
+					 <br:Usuario>mestre</br:Usuario>
+			         <br:Senha>totvs</br:Senha>
+				  </br:ReadRecordAuth>
+			   </soapenv:Body>
+			</soapenv:Envelope>";
+
+		[Fact]
+		public async void Test01()
+		{
+			// GIVEN
+			StringContent content = new StringContent(Envelope, Encoding.UTF8, "text/xml");
+			HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://10.51.5.196/totvsbusinessconnect/wsDataServer.asmx");
+			request.Headers.Add("soapaction", "http://www.totvs.com.br/br/ReadRecordAuth");
+			request.Content = content;
+
+			// WHEN
+			HttpClient client = new HttpClient();
+			HttpResponseMessage response = await client.SendAsync(request);
+			string result = response.Content.ReadAsStringAsync().Result;
+			
+
+
+			XmlDocument xmlDocument = new XmlDocument();
+			xmlDocument.LoadXml(result);
+			xmlDocument.GetElementsByTagName("ReadRecordAuthResult");
+			XmlNodeList list = xmlDocument.GetElementsByTagName("ReadRecordAuthResult");
+			XmlNode node = list.Item(0);
+			string ReadRecordAuthResult = node.InnerText;
+
+			// THEN
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			Assert.Contains("CLARICE PINTO", result);
+		}
+
+
+		//Outra forma de fazer
+		//request.Method = HttpMethod.Post;
+		//request.RequestUri = new Uri("http://10.51.5.196/totvsbusinessconnect/wsDataServer.asmx");
+	}
+}
+
+
